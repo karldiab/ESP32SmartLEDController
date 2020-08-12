c495849c3a97bb5ea0fea1b1ccf3095f4d7d9d9d
karldiab <karl@karldiab.com>
2020-08-12 14:57:25 -0700

    First commit. Board functions to control LED strip with demo LED routine and displays 'hello' on LCD. Will now work on changing LED routine and incorperating motion sensors

---
+++ ESP32SmartLEDController.ino
@@ -0,0 +1,185 @@
+//display shit
+#include "SPI.h"
+#include "TFT_eSPI.h"
+
+// The custom font file attached to this sketch must be included
+#include "MyFont.h"
+
+// Stock font and GFXFF reference handle
+#define GFXFF 1
+
+// Easily remembered name for the font
+#define MYFONT32 &myFont32pt8b
+// Use hardware SPI
+TFT_eSPI tft = TFT_eSPI();
+
+
+//LED STRIP SHIT
+#include <Adafruit_NeoPixel.h>
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    12
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 100
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup(void) {
+
+  Serial.begin(250000);
+  
+  tft.begin();
+
+  tft.setRotation(1);
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+}
+
+void loop() {
+
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // Show custom fonts
+  // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  // Where font sizes increase the screen is not cleared as the larger fonts overwrite
+  // the smaller one with the background colour.
+
+  // We can set the text datum to be Top, Middle, Bottom vertically and Left, Centre
+  // and Right horizontally. These are the text datums that can be used:
+  // TL_DATUM = Top left (default)
+  // TC_DATUM = Top centre
+  // TR_DATUM = Top right
+  // ML_DATUM = Middle left
+  // MC_DATUM = Middle centre <<< This is used below
+  // MR_DATUM = Middle right
+  // BL_DATUM = Bottom left
+  // BC_DATUM = Bottom centre
+  // BR_DATUM = Bottom right
+  // L_BASELINE = Left character baseline (Line the 'A' character would sit on)
+  // C_BASELINE = Centre character baseline
+  // R_BASELINE = Right character baseline
+
+  //Serial.println();
+
+//  // Set text datum to middle centre (MC_DATUM)
+//  tft.setTextDatum(MC_DATUM);
+//
+//  // Set text colour to white with black background
+//  // Unlike the stock Adafruit_GFX library, the TFT_eSPI library DOES optionally draw
+//  // the background colour for the custom and Free Fonts when using drawString()
+//  tft.setTextColor(TFT_WHITE, TFT_BLACK);      // White characters on black background
+//  //tft.setTextColor(TFT_WHITE);               // or white characters, no background
+//
+//  tft.fillScreen(TFT_BLUE);                    // Clear screen
+//  tft.setFreeFont(MYFONT32);                   // Select the font
+//  tft.drawString("MyFont 32", 160, 60, GFXFF); // Print the name of the font
+  tft.setFreeFont(MYFONT32);                   // Select the font
+//  tft.drawString(TEST_TEXT, 160, 140, GFXFF);  // Print the test text in the custom font
+//  delay(2000);
+
+  // Setting textDatum does nothing when using tft.print
+  tft.fillScreen(TFT_BLUE);  // Clear screen
+  tft.setCursor(0,60);       // To be compatible with Adafruit_GFX the cursor datum is always bottom left
+  tft.print("Hello");         // Using tft.print means text background is NEVER rendered
+  
+  // Reset text padding to zero (default)
+  tft.setTextPadding(0);
+
+
+  //LEDS
+  // Fill along the length of the strip in various colors...
+  colorWipe(strip.Color(255,   0,   0), 50); // Red
+  colorWipe(strip.Color(  0, 255,   0), 50); // Green
+  colorWipe(strip.Color(  0,   0, 255), 50); // Blue
+
+  // Do a theater marquee effect in various colors...
+  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
+  theaterChase(strip.Color(127,   0,   0), 50); // Red, half brightness
+  theaterChase(strip.Color(  0,   0, 127), 50); // Blue, half brightness
+
+  rainbow(10);             // Flowing rainbow cycle along the whole strip
+  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
+}
+
+
+// Some functions of our own for creating animated effects -----------------
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<30; a++) {  // Repeat 30 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
+  }
+}
